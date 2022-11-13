dafe41d98a7658a5907781f6925661e10337b7f2
Hiroshi Miura <miurahr@linux.com>
2022-11-13 12:10:37 +0900

    Work in progress
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- zipfile_inflate64/__init__.py
+++ zipfile_inflate64/__init__.py
@@ -3,12 +3,43 @@
 # Has the side effect of applying patches
 from . import _zipfile  # noqa: F401
 
-# Re-export everything, so this can be used in place of zipfile
-from zipfile import *  # noqa: F401, F403
+from zipfile import (
+    BadZipFile,
+    BadZipfile,
+    error,
+    ZIP_STORED,
+    ZIP_DEFLATED,
+    ZIP_BZIP2,
+    ZIP_LZMA,
+    is_zipfile,
+    ZipInfo,
+    ZipFile,
+    PyZipFile,
+    LargeZipFile,
+    Path,
+)
 
-# ZIP_DEFLATED64 is not a part of zipfile.__all__
-from zipfile import ZIP_DEFLATED64  # type: ignore[attr-defined] # noqa: F401
+from zipfile import ZIP_DEFLATED64  # type: ignore[attr-defined]  # noqa: F401
 
 from zipfile_inflate64.version import __version__
 
-__copyright__ = "Copyright (C) 2022 Hiroshi Miura"
+__copyright__ = 'Copyright (C) 2022 Hiroshi Miura'
+
+__all__ = [
+    'BadZipFile',
+    'BadZipfile',
+    'error',
+    'ZIP_STORED',
+    'ZIP_DEFLATED',
+    'ZIP_BZIP2',
+    'ZIP_LZMA',
+    'ZIP_DEFLATED64',
+    'is_zipfile',
+    'ZipInfo',
+    'ZipFile',
+    'PyZipFile',
+    'LargeZipFile',
+    'Path',
+    '__version__',
+    '__copyright__',
+]
