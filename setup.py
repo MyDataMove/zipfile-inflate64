02fb33bcd7b838ba0c946cf9d53d671a37754976
Brad Arndt <brad.arndt@gmail.com>
2024-06-10 13:15:52 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='zipfile-inflate64',
+    version='0.7.0',
+    packages=setuptools.find_packages(),
+    description='Patch ZipFile to support inflate64 compression',
+    url='https://github.com/MyDataMove/zipfile-inflate64',
+    license='Apache License, Version 2.0',
+    setup_requires=[
+    ],
+)
